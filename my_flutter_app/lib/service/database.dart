d113100680538e7895ef036e5bb9e04c2b83325a
Nguyễn Thành Đạt 20130225 <116094166+nguyenthanhdat20130225@users.noreply.github.com>
2025-01-15 04:25:40 +0700

    Order
    Order

--- my_flutter_app/lib/service/database.dart
+++ my_flutter_app/lib/service/database.dart
@@ -1,18 +1,44 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_core/firebase_core.dart';
 
-class DatabaseMethods{
-  Future addUserDetail(Map<String, dynamic> userInfoMap, String id) async{
-    return await FirebaseFirestore.instance.collection('USERS').doc(id).set(userInfoMap);
+class DatabaseMethods {
+  Future addUserDetail(Map<String, dynamic> userInfoMap, String id) async {
+    return await FirebaseFirestore.instance
+        .collection('USERS')
+        .doc(id)
+        .set(userInfoMap);
   }
 
-  UpdateUserWallet(String id, String amount) async{
-    return await FirebaseFirestore.instance.collection('USER').doc(id).update({"Wallet": amount});
+  UpdateUserWallet(String id, String amount) async {
+    return await FirebaseFirestore.instance
+        .collection('USER')
+        .doc(id)
+        .update({"Wallet": amount});
   }
 
   Future addFoodItem(Map<String, dynamic> foodItemData) async {
     // Thêm món ăn vào collection 'foodItems'
-    return await FirebaseFirestore.instance.collection('foodItems').add(foodItemData);
+    return await FirebaseFirestore.instance
+        .collection('foodItems')
+        .add(foodItemData);
   }
 
+  Future<Stream<QuerySnapshot>> getFoodCart(String name) async {
+    return await FirebaseFirestore.instance.collection('name').snapshots();
+  }
+
+  Future addFoodtoCart(Map<String, dynamic> userInfoMap, String id) async {
+    return await FirebaseFirestore.instance
+        .collection("users")
+        .doc(id)
+        .collection("Cart")
+        .add(userInfoMap);
+  }
+
+  Future<Stream<QuerySnapshot>> getFoodItem(String id) async {
+    return await FirebaseFirestore.instance
+        .collection('users')
+        .doc(id)
+        .collection('Cart')
+        .snapshots();
+  }
 }
